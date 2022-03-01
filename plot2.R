55aaf40cf25ce14571d4b29ee5cb29dc83239bb2
shrinu7 <88055808+shrinu7@users.noreply.github.com>
2022-03-01 16:07:49 +0530

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,14 @@
+library("data.table")
+
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+
+
+mydata <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+mydata[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+mydata[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+mydata <- mydata[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+
+plot(x = powerDT[, dateTime], y = powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
